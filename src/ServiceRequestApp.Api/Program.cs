4f09fc18c579be25b134401153f74746ee6c205e
Prof. André Luiz Braga, D.Sc <andre.luiz.braga2000@gmail.com>
2026-02-18 12:48:48 -0300

    Day 2 - Afternoon - Adding DbContext and Migrations

--- src/ServiceRequestApp.Api/Program.cs
+++ src/ServiceRequestApp.Api/Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using ServiceRequestApp.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Swagger / OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+
+// DbContext registration (SQL Server)
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(  
+   builder.Configuration.GetConnectionString("DefaultConnection"))  
+);
+
 var app = builder.Build();
 
 app.MapGet("/status", () => "OK");
-
 app.Run();
